b5344489719b333f555e58b383680848bec5a3a9
abdulaziz1999 <azizmentor96@gmail.com>
2022-01-03 11:28:54 +0700

    port use .env

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 const router = require('./src/routes/api')
+require('dotenv').config()
+const { APP_PORT } = process.env
+
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use(router)
 
-app.listen(9000,()=>{
+app.listen(APP_PORT,()=>{
  console.log("starting... port : 9000");
 });
